23e89aa73863c5ae00ae9aea86633b57d5ff7fa5
rafi arkana <rafiarkana1122@gmail.com>
2024-11-29 17:25:40 +0700

    Modul5

---
+++ laravel11-api/app/Models/Post.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    /**
+* fillable
+*
+* @var array
+*/
+protected $fillable = [
+    'image',
+    'title',
+    'content',
+    ];
+/**
+ * 
+ * @return Attribute
+*/
+protected function image(): Attribute
+{
+return Attribute::make(
+get: fn ($image) => url('/storage/posts/' . $image),
+);
+}
+    
+}
